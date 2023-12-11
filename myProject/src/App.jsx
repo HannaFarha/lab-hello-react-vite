0bb7fddc12ed3b37d34a426f6e5d50a60fcf5cb5
HannaFarha <johnnyfarha1@gmail.com>
2023-12-11 15:05:00 +0100

    done

---
+++ myProject/src/App.jsx
@@ -0,0 +1,61 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// export default App
+import './App.css';
+import icon1 from './assets/icon1.png';
+import icon2 from './assets/icon2.png';
+import icon3 from './assets/icon3.png';
+import icon4 from './assets/icon4.png';
+import icon5 from './assets/icon5.png';
+
+function App() {
+  return (
+    <div className="App">
+     
+   
+    <img src={icon5} /> 
+    <h1>Say hello to ReactJS</h1>
+    <div>You will learn how to use the most popular frontend library, and become a super Ninja developer.<br></br> <button>Awesome</button>  </div>
+    
+    <img src={icon1} /> 
+    <img src={icon2} /> 
+    <img src={icon3} /> 
+    <img src={icon4} /> 
+    
+    </div>
+  );
+}
+
+export default App;
